2b36725d08d02c723686ee6d8349f37e1c52f3d5
Wikonator <Jebaltopes@gmail.com>
2017-03-13 21:55:31 +0100

    changed user login menu in navbar and icon spacing

---
+++ public/javascripts/popup.js
@@ -0,0 +1,30 @@
+(function($){
+	$.fn.styleddropdown = function(){
+		return this.each(function(){
+			var obj = $(this)
+			obj.find(".field").click(function() {
+			obj.find(".list").fadeIn(400);
+
+			$(document).keyup(function(event) {
+				if(event.keyCode == 27) { //press escape
+				obj.find(".list").fadeOut(400);
+				}
+			});
+
+			obj.find(".list").hover(function(){ },
+				function(){
+					$(this).fadeOut(400);
+				});
+			});
+
+			obj.find(".list li").click(function() {
+				console.log("I clickd");
+			obj.find(".list").fadeOut(400);
+			});
+		});
+	};
+})(jQuery);
+
+$(function(){
+	$(".size").styleddropdown();
+});
