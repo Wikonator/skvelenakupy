01aced1f9963b2d2c242e5daaa38e3eb8a30401f
Wikonator <topes.jebal@gmail.com>
2017-08-30 17:33:27 +0200

    removing config again

--- models/config.js
+++ models/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    passportSecret: "Brucewayneisthebatman",
+    passportSecret: "wabbaLubbaDubDub",
     brainMerchantId: "x6nvgtc7dwjwz2wy",
     brainPublicKey: "vf6jdwsw9vfzd3kk",
     brainPrivateKey: "a0d5b778237c2ed0d73cc11d5e0fea41",
